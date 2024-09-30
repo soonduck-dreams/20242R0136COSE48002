ac4e13316b2d1ee46999a10497a67667b2346d9e
soonduck <nederileni99heits@gmail.com>
2024-09-30 17:15:54 +0900

    Initiall commit

---
+++ harmonynow_backend/src/main/java/jhhan/harmonynow_backend/HarmonynowBackendApplication.java
@@ -0,0 +1,13 @@
+package jhhan.harmonynow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonynowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonynowBackendApplication.class, args);
+	}
+
+}
---
+++ harmonynow_backend/src/test/java/jhhan/harmonynow_backend/HarmonynowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jhhan.harmonynow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonynowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
